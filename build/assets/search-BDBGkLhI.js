e14bda380146646909785d795a69b95abb7387e1
Antony Geldof <geldofa@gmail.com>
2025-12-10 12:56:49 +0100

    fix(css): use correct css entry point
    - Change css import in main.tsx to globals.css
    - Remove stale index.css file

--- build/assets/search-BDBGkLhI.js
+++ build/assets/search-BDBGkLhI.js
@@ -1,4 +1,4 @@
-import{c}from"./index-DODHfEcU.js";/**
+import{c}from"./index-BIS4mEZ5.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- build/assets/square-pen-Cv7Cmfe7.js
+++ build/assets/square-pen-Cv7Cmfe7.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DODHfEcU.js";/**
+import{c as a}from"./index-BIS4mEZ5.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
